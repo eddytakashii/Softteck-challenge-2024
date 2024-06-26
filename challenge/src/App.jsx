a59734f055dadda119050e5e5e934ea1e093e32b
Eddy Takashi <Nakauchi.takashi@gmail.com>
2024-06-26 15:03:59 -0300

    first commit

---
+++ challenge/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header"
+import Chat from "./components/Chat"
+const App =()=>{
+  return <div>
+    <Header/>
+    <Chat/>
+  </div>
+  
+}
+
+export default App
---
+++ challenge/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+const Header =()=>{
+    return <div className="bg-gray-500 m-auto flex gap-10">
+        <span>Ola usuario</span>
+        <h1>Chatbot</h1>
+    </div>
+}
+
+export default Header
